5b94c7334d0ede1e2e36f6fcf125a302a9eaa6f4
Yusril A. P <yusriladr.37@gmail.com>
2022-03-29 18:42:19 +0700

    Change app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Selamat Datang di Website Static Yusril Adriansyah Putra.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/yusril-adr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Saya
         </a>
       </header>
     </div>
